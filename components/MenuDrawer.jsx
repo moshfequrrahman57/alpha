0b59c6bfd5114aa9d5286f2dcfc212bbffaca5ec
Moshfequr <moshfequrrahman57@gmail.com>
2025-06-01 18:43:26 +0600

    navbar almost done

--- components/MenuDrawer.jsx
+++ components/MenuDrawer.jsx
@@ -16,6 +16,7 @@ function MenuDrawer() {
     const list_div=useRef();
     const demos_list_div=useRef();
     const all_pages_list=useRef();
+    const company_div=useRef();
    
    const [isOpen,setIsOpen]=useState(false);
     const drawer=()=>{
@@ -53,9 +54,13 @@ function MenuDrawer() {
 
   
 
-     useEffect(() => {
-      const clone_demos=demos_list_div.current.cloneNode(true);
-      all_pages_list.current.appendChild(clone_demos);
+     useEffect(  () => {
+      // const clone_demos=demos_list_div.current.cloneNode(true);
+      // all_pages_list.current.appendChild(clone_demos);
+      // const clone_company=company_div.current.cloneNode(true);
+      // all_pages_list.current.appendChild(clone_company);
+
+     
          const get_break_point=()=>{
 
       const breakpoints={
@@ -100,151 +105,233 @@ function MenuDrawer() {
 
     return (
         
-        <nav className="bg-gray-100 shadow w-full flex justify-between items-center" >
+        <nav className=" shadow w-full  flex justify-between items-center" >
          
-          <div className='flex items-center justify-center mx-3'>
+          <div className='flex items-center justify-center mx-3 h-16 md:h-20'>
             <Link href={'/'} className='flex items-center justify-center'>
              <Image
-            src="/medium-color-svgrepo-com.svg"
-            width={50}
-            height={50}
+            src="/speech-bubble-svgrepo-com (1).svg"
+            width={40}
+            height={40}
             alt="Logo"
             />
-        <span className='mx-3 text-2xl font-extrabold text-blue-300'>Alpha</span>
+        <span className='mx-3 text-2xl font-extrabold text-[#007aff]'>alpha</span>
             </Link>
           </div>
 
-         <ul className={`fixed top-0 md:top-0 left-0 h-full w-3/5 bg-gray-200 shadow-lg transform transition-transform duration-300 ease-in-out
-          md:relative md:flex  gap-4 md:bg-gray-600 md:h-fit md:w-fit md:translate-x-0
+         <ul className={`fixed top-0 md:top-0 left-0 h-full w-3/4 bg-white  transform 
+         transition-transform duration-300 ease-in-out shadow md:shadow-none
+          md:relative md:flex  gap-4  md:h-20 md:w-fit md:translate-x-0
           ${isOpen ? 'translate-x-0' : '-translate-x-full'}
          
           `}>
-            <div ref={list_div} className='flex flex-col md:flex-row  gap-0 pt-10 pl-10 md:pt-0 md:pl-0 bg-pink-500'>
+
+          <div  className='flex justify-between items-center   cursor-pointer h-16 shadow md:hidden'
+                >
+                  <div className='flex items-center justify-center gap-4 mx-3'>
+                     <Image
+                  src="/speech-bubble-svgrepo-com (1).svg"
+                  width={40}
+                  height={40}
+                  alt="Logo"
+                  />
+                <span className="text-2xl font-extrabold text-[#007aff] md:text-base" 
+                >alpha</span>
+                  </div>
+                 
+                <Image
+                  src="/close-svgrepo-com.svg"
+                  width={40}
+                  height={40}
+                  alt="Logo"
+                  onClick={drawer}
+                  />
+                </div>
+
+            <div ref={list_div} className='flex flex-col md:flex-row md:items-center md:justify-center   pt-5 pl-5 md:pt-0 md:pl-0 '>
             
-              <div   className='md:relative h-fit' >
-                <div  className='flex justify-between items-center  bg-red-400 cursor-pointer'
+              <div   className='md:relative h-fit bg-white' >
+                <div  className='flex justify-between items-center cursor-pointer h-fit mr-5'
                 
                     onClick={expand_demos} 
                 >
-                <span className="text-2xl font-bold md:text-base hover:text-violet-500 " 
+                <span className="text-2xl font-semibold md:text-base h-16 flex text-gray-500
+                 hover:md:text-[#007aff]   items-center justify-center" 
                 onMouseOver={expand_demos} onMouseOut={expand_demos}
                 >Demos</span>
                 <Image
                   src="/dropdown-arrow-svgrepo-com.svg"
-                  width={50}
-                  height={50}
+                  width={30}
+                  height={30}
                   alt="Logo"
+                
                   />
                 </div>
-                <ul className={`md:absolute md:top-14 md:left-0 bg-green-300 transition-all  duration-700
-                ${isDemosOpen ? ' h-40':'h-0 md:hidden '}  
+                <ul className={`md:absolute md:top-16 md:w-48 md:left-0 md:border
+                 md:border-slate-200 md:shadow
+                 transition-all  duration-700 
+                ${isDemosOpen ? ' h-64':'h-0 md:hidden '}  
                 `}
                  
                 >
-                  <div ref={demos_list_div} >
-                  <li>Home 1</li>
-                  <li>Home 2</li>
-                  <li>Home 3</li>
-                  <li>Home 4</li>
-                  <li>Home 5</li>
-                  <li>Home 6</li>
+                  <div ref={demos_list_div} onMouseOver={()=>{ setDemosOpen(true)}} 
+                  onMouseOut={()=>{ setDemosOpen(false)}} className='flex flex-col gap-2 p-5 bg-white'>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 1</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 2</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 3</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 4</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 5</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 6</li>
                   </div>
                 </ul>
 
               </div>
             
              
-               <div id='product_div' className='md:relative h-fit' >
-                <div  className='flex justify-between items-center  bg-red-400 cursor-pointer'
+               <div id='product_div' className='md:relative h-fit bg-white' >
+                <div  className='flex justify-between items-center cursor-pointer h-fit mr-5'
                 
                     onClick={expand_product} 
                 >
-                <span className="text-2xl font-bold md:text-base hover:text-violet-500 " 
+                <span className="text-2xl font-semibold md:text-base h-16 flex text-gray-500
+                 hover:md:text-[#007aff]   items-center justify-center"
                 onMouseOver={expand_product} onMouseOut={expand_product}
                 >Product Features</span>
                 <Image
                   src="/dropdown-arrow-svgrepo-com.svg"
-                  width={50}
-                  height={50}
+                  width={30}
+                  height={30}
                   alt="Logo"
                   />
                 </div>
-                <ul className={`md:absolute md:top-14 md:left-0 bg-green-300 transition-all  duration-700
-                ${isProductOpen ? 'h-52':'h-0 md:hidden'}  
+                <ul className={`md:absolute md:top-16 md:w-48 md:left-0 md:border
+                 md:border-slate-200 md:shadow
+                 transition-all  duration-700 
+                ${isProductOpen ? 'h-64':'h-0 md:hidden'}  
                 `}
                  
                 >
-                  <li>Features</li>
-                  <li>Compare</li>
-                  <li>Pricing</li>
-                  <li>Reviews</li>
-                  <li>Get Support</li>
-                  
+
+                    <div onMouseOver={()=>{ setProductOpen(true)}} 
+                  onMouseOut={()=>{ setProductOpen(false)}} className='flex flex-col gap-2 p-5 bg-white'>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Features</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Compare</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Pricing</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Reviews</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Get Support</li>
+                  </div>
                 </ul>
 
               </div>
             
-                <div id='company_div' className='md:relative h-fit' >
-                <div  className='flex justify-between items-center  bg-red-400 cursor-pointer'
+                <div  className='md:relative h-fit bg-white' >
+                <div className='flex justify-between items-center cursor-pointer h-fit mr-5'
                 
                     onClick={expand_company} 
                 >
-                <span className="text-2xl font-bold md:text-base hover:text-violet-500 " 
+                <span className="text-2xl font-semibold md:text-base h-16 flex text-gray-500
+                 hover:md:text-[#007aff]   items-center justify-center"
                 onMouseOver={expand_company} onMouseOut={expand_company}
                 >Company</span>
                 <Image
                   src="/dropdown-arrow-svgrepo-com.svg"
-                  width={50}
-                  height={50}
+                  width={30}
+                  height={30}
                   alt="Logo"
                   />
                 </div>
-                <ul className={`md:absolute md:top-14 md:left-0 bg-green-300 transition-all  duration-700
-                ${isCompanyOpen ? 'h-52':'h-0 md:hidden'}  
+                <ul className={`md:absolute md:top-16 md:w-48 md:left-0 md:border
+                 md:border-slate-200 md:shadow
+                 transition-all  duration-700 
+                ${isCompanyOpen ? 'h-64':'h-0 md:hidden'}  
                 `}
                  
                 >
-                  <li>About Us</li>
-                  <li>Why Choose Us</li>
-                  <li>Our Team</li>
-                  <li>Services</li>
-                  <li>Career</li>
-                   <li>Contact us</li>
+                   <div onMouseOver={()=>{ setCompanyOpen(true)}} 
+                  onMouseOut={()=>{ setCompanyOpen(false)}} className='flex flex-col gap-2 p-5 bg-white'>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>About Us</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Why Choose Us</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Our Team</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Services</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Career</li>
+                   <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Contact us</li>
+                   </div>
                 </ul>
 
               </div>
             
+            <div className='w-full bg-white md:w-fit mr-5 py-5 md:pl-0'>
+              <span className='text-2xl md:text-base font-semibold text-gray-500 hover:md:text-[#007aff]'> Show Cases</span>
+            </div>
       
-              <div  className='md:relative h-fit' >
-                <div  className='flex justify-between items-center  bg-red-400 cursor-pointer'
+              <div  className=' h-fit bg-white' >
+                <div   className='flex justify-between items-center cursor-pointer h-fit mr-5'
                 
                     onClick={expand_all_pages} 
                 >
-                <span className="text-2xl font-bold md:text-base hover:text-violet-500 " 
+                <span className="text-2xl font-semibold md:text-base h-16 flex text-gray-500
+                 hover:md:text-[#007aff]   items-center justify-center"
                 onMouseOver={expand_all_pages} onMouseOut={expand_all_pages}
                 >All Pages</span>
                 <Image
                   src="/dropdown-arrow-svgrepo-com.svg"
-                  width={50}
-                  height={50}
+                  width={30}
+                  height={30}
                   alt="Logo"
                   />
                 </div>
-                <ul ref={all_pages_list} className={`md:absolute md:top-14 md:left-0 bg-green-300 transition-all  duration-700
-                ${isAllPagesOpen ? 'h-52':'h-0 md:hidden'}  
+                <ul ref={all_pages_list} className={`md:absolute md:top-16 md:w-fit md:left-0 md:border
+                 md:border-slate-200 md:shadow
+                 transition-all  duration-700 
+                ${isAllPagesOpen ? 'h-64 md:h-fit':'h-0 md:hidden'} overflow-scroll  md:overflow-hidden
                 `}
-                 
                 >
+               <div onMouseOver={()=>{ setAllPagesOPen(true)}} 
+                  onMouseOut={()=>{ setAllPagesOPen(false)}} className='
+                md:grid md:grid-cols-4 md:gap-4 md:w-[600px] bg-white flex flex-col gap-2 p-5'>    
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Gallery</li>
+                 <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Gallery</li>
+                 <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Gallery</li>
+                 <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Gallery</li>
+                 <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Gallery</li>
+                 <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Gallery</li>
+                      <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 1</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 2</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 3</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 4</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 5</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Home 6</li>
+                   <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Features</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Compare</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Pricing</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Reviews</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Get Support</li>
+                   <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>About Us</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Why Choose Us</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Our Team</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Services</li>
+                  <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Career</li>
+                   <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Contact us</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                    <li className='text-2xl md:text-base font-semibold text-gray-500 hover:text-blue-400'>Hello 1</li>
+                
+                  </div>
                   
                 </ul>
 
               </div>
               
               
+            
 
-
-           <div className='h-40 md:h-fit bg-amber-400'>
-             <button ref={menu_button} className='btn btn-success rounded-full' >Get Started</button>
+           <div ref={menu_button}  className='h-64 md:h-fit flex flex-col bg-white pr-10 py-10
+           md:py-0 '>
+             <button className='btn btn-success rounded-full text-white hover:bg-white hover:text-[#007aff]' >Get Started</button>
            </div>
             </div>
           
